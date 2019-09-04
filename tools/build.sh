db8cb3222d5459126eda8e867d5d5cac4b43baf0
YanQinghao <woshiyanqinghao@gmail.com>
2019-09-04 14:18:14 +0800

    build docker

---
+++ tools/build.sh
@@ -0,0 +1,9 @@
+docker build -t registry.cn-shanghai.aliyuncs.com/shuzhi/docker_wly:$1 -f docker/docker_wly/Dockerfile .
+docker build -t registry.cn-shanghai.aliyuncs.com/shuzhi/docker_wly_gpu:$1 -f docker/docker_wly_gpu/Dockerfile .
+docker build -t registry.cn-shanghai.aliyuncs.com/shuzhi/stream_wly:$1 -f docker/stream_wly/Dockerfile .
+docker build -t registry.cn-shanghai.aliyuncs.com/shuzhi/stream_wly_gpu:$1 -f docker/stream_wly_gpu/Dockerfile .
+
+docker push registry.cn-shanghai.aliyuncs.com/shuzhi/docker_wly:$1
+docker push registry.cn-shanghai.aliyuncs.com/shuzhi/docker_wly_gpu:$1
+docker push registry.cn-shanghai.aliyuncs.com/shuzhi/stream_wly:$1
+docker push registry.cn-shanghai.aliyuncs.com/shuzhi/stream_wly_gpu:$1
