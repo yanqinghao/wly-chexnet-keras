a12dbd369a41949c2d9cb3c6aa08c1d336daf944
YanQinghao <woshiyanqinghao@gmail.com>
2020-06-08 15:27:16 +0800

    fix model train

--- modeltrain/train.py
+++ modeltrain/train.py
@@ -2,47 +2,43 @@
 from __future__ import absolute_import, print_function
 from __suanpan__ import tensorflow
 
-from suanpan.docker import DockerComponent as dc
-from suanpan.docker.arguments import Folder, String, Float
-from keras.optimizers import Adam
-from keras.utils import multi_gpu_model
-from keras.preprocessing.image import ImageDataGenerator
 import os
-from weights import get_class_weights
-from utility import get_sample_counts
-from keras.callbacks import ModelCheckpoint, TensorBoard, ReduceLROnPlateau, CSVLogger
-from callback import MultipleClassAUROC, MultiGPUModelCheckpoint
-from callbackrealtime import CSVLoggerOss
-from model import ModelFactory
-import json
-from suanpan.storage import storage
-import pandas as pd
-import shutil
 import time
+import json
+import shutil
+import pandas as pd
 import tensorflow as tf
 from tensorflow.python.client import device_lib
 from keras import backend as K
+from keras.optimizers import Adam
+from keras.utils import multi_gpu_model
+from keras.preprocessing.image import ImageDataGenerator
+import suanpan
+from suanpan.app import app
+from suanpan.storage import storage
+from suanpan.log import logger
+from suanpan.docker.arguments import Folder, String, Float
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, CSVLogger
+from callback import MultiGPUModelCheckpoint
+from callbackrealtime import CSVLoggerOss
+from model import ModelFactory
 
 
-# 定义输入
-@dc.input(Folder(key="inputData1", required=True))
-@dc.param(String(key="param1"))
-@dc.param(Float(key="param2"))
-# 定义输出
-@dc.output(Folder(key="outputData1", required=True))
+@app.input(Folder(key="inputData1", required=True))
+@app.param(String(key="param1"))
+@app.param(Float(key="param2"))
+@app.output(Folder(key="outputData1", required=True))
 def Demo(context):
-    # 从 Context 中获取相关数据
     args = context.args
-    # 查看上一节点发送的 args.inputData1 数据
-    print(args.inputData1)
+
     config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
     sess = tf.Session(config=config)
 
-    print("GPU device:", tf.test.gpu_device_name())
-    print("is GPU available:", tf.test.is_gpu_available())
-    print("list all devices:", device_lib.list_local_devices())
-    print("check all devices:", K.tensorflow_backend._get_available_gpus())
+    logger.info("GPU device:", tf.test.gpu_device_name())
+    logger.info("is GPU available:", tf.test.is_gpu_available())
+    logger.info("list all devices:", device_lib.list_local_devices())
+    logger.info("check all devices:", K.tensorflow_backend._get_available_gpus())
 
     modelName = "DenseNet121"
     downloadPath = "/tmp/images"
@@ -60,13 +56,10 @@ def Demo(context):
     }
 
     if not os.path.exists(
-        "/root/.keras/models/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5"
-    ):
+            "/root/.keras/models/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5"):
         os.makedirs("/root/.keras/models")
-        if os.path.exists(
-            "/wly/model/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5"
-        ):
-            print("******************Load local model******************")
+        if os.path.exists("/wly/model/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5"):
+            logger.info("Load local model")
             shutil.copyfile(
                 "/wly/model/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5",
                 "/root/.keras/models/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5",
@@ -76,36 +69,35 @@ def Demo(context):
                 "common/model/keras/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5",
                 "/root/.keras/models/densenet121_weights_tf_dim_ordering_tf_kernels_notop.h5",
             )
-    # 自定义代码
     outputDir = args.outputData1
     if os.path.exists(downloadPath):
         shutil.rmtree(downloadPath)
     if os.path.exists(imagePath):
         shutil.rmtree(imagePath)
 
-    if args.param1 == None:
+    if args.param1 is None:
         trainDir = os.path.join(args.inputData1, "train")
         validationDir = os.path.join(args.inputData1, "test")
     else:
         ossPath = args.param1
         with open("training_log.json", "w") as f:
             json.dump({"info": "downloadmodel", "data": None}, f)
-        if os.path.split(ossPath)[0] != None:
+        if os.path.split(ossPath)[0] is not None:
             storage.upload(
                 "{}/training_log.json".format(os.path.split(ossPath)[0]),
                 "training_log.json",
             )
-        trainTestSplit = args.param2 if args.param2 != None else 0.8
-        print("************Split train and test : {}***********".format(trainTestSplit))
+        trainTestSplit = args.param2 if args.param2 is not None else 0.8
+        logger.info("Split train and test : {}".format(trainTestSplit))
         with open("training_log.json", "w") as f:
             json.dump({"info": "downloadimage", "data": None}, f)
-        if os.path.split(ossPath)[0] != None:
+        if os.path.split(ossPath)[0] is not None:
             storage.upload(
                 "{}/training_log.json".format(os.path.split(ossPath)[0]),
                 "training_log.json",
             )
         storage.download(ossPath, downloadPath)
-        print(os.path.split(ossPath)[0] + "/detail.json")
+        logger.info(os.path.split(ossPath)[0] + "/detail.json")
         storage.download(os.path.split(ossPath)[0] + "/detail.json", "detail.json")
         with open("detail.json", "r") as f:
             label_detail = json.load(f)
@@ -125,11 +117,11 @@ def Demo(context):
         for i in list(set(labels["level"].values)):
             os.mkdir(os.path.join(imagePath, "train", i))
             os.mkdir(os.path.join(imagePath, "test", i))
-        print(list(set(labels["level"].values)))
+        logger.info(list(set(labels["level"].values)))
         foldercheck = []
         with open("training_log.json", "w") as f:
             json.dump({"info": "imageformat", "data": None}, f)
-        if os.path.split(ossPath)[0] != None:
+        if os.path.split(ossPath)[0] is not None:
             storage.upload(
                 "{}/training_log.json".format(os.path.split(ossPath)[0]),
                 "training_log.json",
@@ -137,20 +129,16 @@ def Demo(context):
         for m in list(set(labels["level"].values)):
             labels_tmp = labels[labels["level"] == m]
             foldercheck.append(len(labels_tmp) * trainTestSplit > (len(labels_tmp) - 1))
-            print(labels_tmp["level"])
+            logger.info(labels_tmp["level"])
             for j in range(len(labels_tmp)):
                 if j < len(labels_tmp) * trainTestSplit:
-                    print(os.path.join(imagePath, "train", labels_tmp["level"].iloc[j]))
-                    print(os.path.join(downloadPath, labels_tmp["name"].iloc[j]))
-                    src = os.path.join(
-                        downloadPath, labels_tmp["name"].iloc[j] + ".png"
-                    )
+                    logger.info(os.path.join(imagePath, "train", labels_tmp["level"].iloc[j]))
+                    logger.info(os.path.join(downloadPath, labels_tmp["name"].iloc[j]))
+                    src = os.path.join(downloadPath, labels_tmp["name"].iloc[j] + ".png")
                     dst = os.path.join(imagePath, "train", labels_tmp["level"].iloc[j])
                     shutil.copy(src, os.path.join(dst, str(j) + os.path.split(src)[1]))
                 else:
-                    src = os.path.join(
-                        downloadPath, labels_tmp["name"].iloc[j] + ".png"
-                    )
+                    src = os.path.join(downloadPath, labels_tmp["name"].iloc[j] + ".png")
                     dst = os.path.join(imagePath, "test", labels_tmp["level"].iloc[j])
                     shutil.copy(src, os.path.join(dst, str(j) + os.path.split(src)[1]))
         trainDir = os.path.join(imagePath, "train")
@@ -170,7 +158,7 @@ def Demo(context):
 
     with open("training_log.json", "w") as f:
         json.dump({"info": "loadmodel", "data": None}, f)
-    if os.path.split(ossPath)[0] != None:
+    if os.path.split(ossPath)[0] is not None:
         storage.upload(
             "{}/training_log.json".format(os.path.split(ossPath)[0]),
             "training_log.json",
@@ -191,80 +179,66 @@ def Demo(context):
         zoom_range=0.15,
         rescale=1.0 / 255,
     )
-    trainGenerator = (
-        trainDatagen.flow_from_directory(
-            trainDir,
-            target_size=(imageSize, imageSize),
-            batch_size=batchSize,
-            class_mode="categorical",
-        )
-        if classNum > 2
-        else trainDatagen.flow_from_directory(
-            trainDir,
-            target_size=(imageSize, imageSize),
-            batch_size=batchSize,
-            class_mode="binary",
-        )
-    )
+    trainGenerator = (trainDatagen.flow_from_directory(
+        trainDir,
+        target_size=(imageSize, imageSize),
+        batch_size=batchSize,
+        class_mode="categorical",
+    ) if classNum > 2 else trainDatagen.flow_from_directory(
+        trainDir,
+        target_size=(imageSize, imageSize),
+        batch_size=batchSize,
+        class_mode="binary",
+    ))
 
     labelMap = trainGenerator.class_indices
     classNames = list(labelMap.keys())
     with open(os.path.join(outputDir, "label_map.json"), "w") as f:
         json.dump(labelMap, f)
-        print("save label map...")
+        logger.info("save label map...")
 
-    # train_counts, train_pos_counts = get_sample_counts(trainDir, "train", classNames)
-    print("** compute class weights from training data **")
-    # classWeights = get_class_weights(train_counts, train_pos_counts, multiply=1)
-    print("** class_weights **")
+    logger.info("compute class weights from training data")
+    logger.info("class_weights")
 
     validationDatagen = ImageDataGenerator(rescale=1.0 / 255)
-    validationGenerator = (
-        validationDatagen.flow_from_directory(
-            validationDir,
-            target_size=(imageSize, imageSize),
-            batch_size=batchSize,
-            class_mode="categorical",
-        )
-        if classNum > 2
-        else validationDatagen.flow_from_directory(
-            validationDir,
-            target_size=(imageSize, imageSize),
-            batch_size=batchSize,
-            class_mode="binary",
-        )
-    )
+    validationGenerator = (validationDatagen.flow_from_directory(
+        validationDir,
+        target_size=(imageSize, imageSize),
+        batch_size=batchSize,
+        class_mode="categorical",
+    ) if classNum > 2 else validationDatagen.flow_from_directory(
+        validationDir,
+        target_size=(imageSize, imageSize),
+        batch_size=batchSize,
+        class_mode="binary",
+    ))
 
-    model = models.get_model(
-        model_name=modelName, class_names=classNames, input_shape=inputShape
-    )
-    print(model.summary())
-    print(len(model.layers))
+    model = models.get_model(model_name=modelName, class_names=classNames, input_shape=inputShape)
+    logger.info(model.summary())
+    logger.info(len(model.layers))
 
     gpus = len(os.getenv("CUDA_VISIBLE_DEVICES", "1").split(","))
     if gpus > 1:
-        print(f"** multi_gpu_model is used! gpus={gpus} **")
+        logger.info(f"multi_gpu_model is used! gpus={gpus}")
         model_train = multi_gpu_model(model, gpus)
         # FIXME: currently (Keras 2.1.2) checkpoint doesn't work with multi_gpu_model
-        checkpoint = MultiGPUModelCheckpoint(
-            filepath=outputWeightsPath, base_model=model, save_best_only=True, verbose=1
-        )
+        checkpoint = MultiGPUModelCheckpoint(filepath=outputWeightsPath,
+                                             base_model=model,
+                                             save_best_only=True,
+                                             verbose=1)
     else:
         model_train = model
-        checkpoint = ModelCheckpoint(
-            outputWeightsPath, monitor="val_acc", save_best_only=True, verbose=1
-        )
+        checkpoint = ModelCheckpoint(outputWeightsPath,
+                                     monitor="val_acc",
+                                     save_best_only=True,
+                                     verbose=1)
 
     optimizer = Adam(lr=learningRate)
     if len(classNames) > 2:
-        model_train.compile(
-            optimizer=optimizer, loss="categorical_crossentropy", metrics=["acc"]
-        )
+        model_train.compile(optimizer=optimizer, loss="categorical_crossentropy", metrics=["acc"])
     else:
-        model_train.compile(
-            optimizer=optimizer, loss="binary_crossentropy", metrics=["acc"]
-        )
-    if args.param1 == None:
+        model_train.compile(optimizer=optimizer, loss="binary_crossentropy", metrics=["acc"])
+    if args.param1 is None:
         csvlog = CSVLogger(os.path.join(args.outputData1, "training_log.csv"))
     else:
         csvlog = CSVLoggerOss("training_log.csv", osspath=os.path.split(args.param1)[0])
@@ -284,7 +258,7 @@ def Demo(context):
 
     with open("training_log.json", "w") as f:
         json.dump({"info": "trainmodel", "data": None}, f)
-    if os.path.split(ossPath)[0] != None:
+    if os.path.split(ossPath)[0] is not None:
         storage.upload(
             "{}/training_log.json".format(os.path.split(ossPath)[0]),
             "training_log.json",
@@ -301,9 +275,7 @@ def Demo(context):
             # class_weight=classWeights,
             workers=8,
             shuffle=False,
-        )
-        if len(classNames) > 2
-        else model_train.fit_generator(
+        ) if len(classNames) > 2 else model_train.fit_generator(
             generator=trainGenerator,
             steps_per_epoch=len(trainGenerator),
             epochs=Epoch,
@@ -312,18 +284,13 @@ def Demo(context):
             callbacks=callbacks,
             workers=8,
             shuffle=False,
-        )
-    )
+        ))
     if os.path.exists(downloadPath):
         shutil.rmtree(downloadPath)
     if os.path.exists(imagePath):
         shutil.rmtree(imagePath)
 
-    # data = history.history
-    # data["epoch"] = list(range(Epoch))
-    # with open("training_log.json", "w") as f:
-    #     json.dump({"info": "uploadmodel", "data": data}, f)
-    if os.path.split(ossPath)[0] != None:
+    if os.path.split(ossPath)[0] is not None:
         storage.upload(
             "{}/training_log.json".format(os.path.split(ossPath)[0]),
             "training_log.json",
@@ -334,14 +301,14 @@ def Demo(context):
     trainingInfo["useTime"] = useTime
     with open("traininginfo.json", "w") as f:
         json.dump(trainingInfo, f)
-    if os.path.split(ossPath)[0] != None:
+    if os.path.split(ossPath)[0] is not None:
         storage.upload(
             "{}/traininginfo.json".format(os.path.split(ossPath)[0]),
             "traininginfo.json",
         )
-    # 将 args.outputData1 作为输出发送给下一节点
+
     return args.outputData1
 
 
 if __name__ == "__main__":
-    Demo()
+    suanpan.run(app)
