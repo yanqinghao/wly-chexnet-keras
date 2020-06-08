58f8d57181700c9fe6299e8a9673fcdbce3341e0
YanQinghao <woshiyanqinghao@gmail.com>
2020-06-08 15:27:43 +0800

    fix build tool

--- tools/build.sh
+++ tools/build.sh
@@ -1,9 +1,21 @@
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/docker_wly:$1 -f docker/docker_wly/Dockerfile .
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/docker_wly_gpu:$1 -f docker/docker_wly_gpu/Dockerfile .
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/stream_wly:$1 -f docker/stream_wly/Dockerfile .
-docker build -t registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/stream_wly_gpu:$1 -f docker/stream_wly_gpu/Dockerfile .
+#!/bin/bash
 
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/docker_wly:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/docker_wly_gpu:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/stream_wly:$1
-docker push registry-vpc.cn-shanghai.aliyuncs.com/shuzhi/stream_wly_gpu:$1
+set -e
+
+VERSION=$(bash tools/version.sh)
+DOCKERBASES=("suanpan-python-sdk", "suanpan-python-sdk-cuda")
+TAG="3.7"
+BUILDNAMES=("docker_wly", "docker_wly_gpu")
+REQUIREMENTS=("requirements.txt", "requirements_gpu.txt")
+NAMESPACE="shuzhi-amd64"
+for ((i = 0; i <= ${#DOCKERBASES[@]}; i++)); do
+    docker build --build-arg NAME_SPACE=${NAMESPACE} --build-arg DOCKER_BASE=${DOCKERBASES[i]} \
+        --build-arg PYTHON_VERSION=${TAG} --build-arg REQUIREMENTS_FILE=${REQUIREMENTS[i]} -t \
+        registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[i]}:${VERSION} \
+        -f docker/docker_wly/Dockerfile .
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[i]}:${VERSION}
+
+    docker tag registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[i]}:${VERSION} \
+        registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[i]}:latest
+    docker push registry-vpc.cn-shanghai.aliyuncs.com/${NAMESPACE}/${BUILDNAMES[i]}:latest
+done
