9aaa0ca4bdd69d70cbf269eaa269b17cb7c2cb3e
YanQinghao <woshiyanqinghao@gmail.com>
2020-06-08 15:26:35 +0800

    fix label

--- interactive/labels.py
+++ interactive/labels.py
@@ -3,62 +3,37 @@ from __future__ import absolute_import, print_function
 
 import json
 import random
-from suanpan.stream import Handler as h
-from suanpan.stream import Stream
-from suanpan.stream.arguments import Json
-from suanpan.interfaces import HasArguments
+import suanpan
+from suanpan import g
+from suanpan.app import app
 from suanpan.storage import storage
+from suanpan.stream.arguments import Json
 
 
-class StreamDemo(Stream):
+@app.input(Json(key="inputData1"))
+@app.output(Json(key="outputData1"))
+def labels(self, context):
+    args = context.args
+    programId = args.inputData1["programId"]
+    if args.inputData1["status"] == "success":
+        ossPath = "studio/{}/share/{}/uploads/{}/detail.json".format(g.userId, g.appId, programId)
+        storage.download(ossPath, "detail.json")
+        with open("detail.json", "r") as f:
+            label_detail = json.load(f)
 
-    # 定义输入
-    @h.input(Json(key="inputData1"))
-    # 定义输出
-    @h.output(Json(key="outputData1"))
-    def call(self, context):
-        # 从 Context 中获取相关数据
-        args = context.args
-        # 查看上一节点发送的 args.inputData1 数据
-        print(args.inputData1)
-        envparam = HasArguments.getArgListFromEnv()
-        userId = envparam[envparam.index("--stream-user-id") + 1]
-        appId = envparam[envparam.index("--stream-app-id") + 1]
-        programId = args.inputData1["programId"]
-        if args.inputData1["status"] == "success":
-            ossPath = "studio/{}/{}/{}/detail.json".format(userId, appId, programId)
-            storage.download(ossPath, "detail.json")
-            with open("detail.json", "r") as f:
-                label_detail = json.load(f)
+        idx = random.randint(0, len(label_detail["images"]) - 1)
+        outputData = args.inputData1
+        if "checkType" in label_detail["images"][idx].keys():
+            outputData["result"].update({"checkType": label_detail["images"][idx]["checkType"]})
+        outputData["result"].update(
+            {"imageSharpnessRate": label_detail["images"][idx]["imageSharpnessRate"]})
+        outputData["result"].update(
+            {"deviceCapabilityRate": label_detail["images"][idx]["deviceCapabilityRate"]})
 
-            idx = random.randint(0, len(label_detail["images"]) - 1)
-            outputData = args.inputData1
-            if "checkType" in label_detail["images"][idx].keys():
-                outputData["result"].update(
-                    {"checkType": label_detail["images"][idx]["checkType"]}
-                )
-            outputData["result"].update(
-                {
-                    "imageSharpnessRate": label_detail["images"][idx][
-                        "imageSharpnessRate"
-                    ]
-                }
-            )
-            outputData["result"].update(
-                {
-                    "deviceCapabilityRate": label_detail["images"][idx][
-                        "deviceCapabilityRate"
-                    ]
-                }
-            )
-
-            self.send(outputData)
-        else:
-            self.send(args.inputData1)
-
-        return None
+        return outputData
+    else:
+        return args.inputData1
 
 
 if __name__ == "__main__":
-    StreamDemo().start()
-
+    suanpan.run(app)
